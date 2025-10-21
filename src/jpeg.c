656fc537b1314910948c48779e8fff40312e367a
Pierre Champion <pierre.champion.pro@gmail.com>
2025-10-21 13:10:09 +0200

    Fix: fixed type of in/out mcus in handling functions

--- src/jpeg.c
+++ src/jpeg.c
@@ -12,46 +12,42 @@
 #include <string.h>
 
 // Converts every sample value of a 8x8 mcu from RGB to YCbCr color format
-void rgb_to_ycbcr(uint16_t *mcu) {
+void rgb_to_ycbcr(uint8_t *mcu_in, uint8_t *mcu_out) {
   for (int j = 0; j < 8; j++) {
     for (int i = 0; i < 8; i++) {
-      uint16_t r = mcu[3 * 8 * j + 3 * i + 0];
-      uint16_t g = mcu[3 * 8 * j + 3 * i + 1];
-      uint16_t b = mcu[3 * 8 * j + 3 * i + 2];
+      uint8_t r = mcu_in[3 * 8 * j + 3 * i + 0];
+      uint8_t g = mcu_in[3 * 8 * j + 3 * i + 1];
+      uint8_t b = mcu_in[3 * 8 * j + 3 * i + 2];
 
-      mcu[3 * 8 * j + 3 * i + 0] = 0.299 * r + 0.587 * g + 0.114 * b;
-      mcu[3 * 8 * j + 3 * i + 1] = -0.167 * r - 0.3313 * g - 0.5 * b + 128;
-      mcu[3 * 8 * j + 3 * i + 2] = 0.5 * r - 0.4187 * g - 0.0813 * b + 128;
+      mcu_out[3 * 8 * j + 3 * i + 0] = 0.299 * r + 0.587 * g + 0.114 * b;
+      mcu_out[3 * 8 * j + 3 * i + 1] = -0.167 * r - 0.3313 * g - 0.5 * b + 128;
+      mcu_out[3 * 8 * j + 3 * i + 2] = 0.5 * r - 0.4187 * g - 0.0813 * b + 128;
     }
   }
 }
 
 // Naive implementation of a 2D DCT
-void dct(uint16_t *mcu) {
-  uint16_t *buf = malloc(3 * 8 * 8 * sizeof(uint16_t));
-  memcpy(buf, mcu, 3 * 8 * 8 * sizeof(uint16_t));
-
+void dct(int8_t *mcu_in, double *mcu_out) {
   for (int j = 0; j < 8; j++) {
     for (int i = 0; i < 8; i++) {
       for (int c = 0; c < 3; c++) {
-        mcu[3 * 8 * j + 3 * i + c] = 0;
+        mcu_out[3 * 8 * j + 3 * i + c] = 0;
 
         for (int y = 0; y < 8; y++) {
           for (int x = 0; x < 8; x++) {
-            mcu[3 * 8 * j + 3 * i + c] += buf[3 * 8 * y + 3 * x + c] *
-                                          cos(((2 * x + 1) * i * M_PI) / 16) *
-                                          cos(((2 * y + 1) * j * M_PI) / 16);
+            mcu_out[3 * 8 * j + 3 * i + c] +=
+                mcu_in[3 * 8 * y + 3 * x + c] *
+                cos(((2 * x + 1) * i * M_PI) / 16) *
+                cos(((2 * y + 1) * j * M_PI) / 16);
           }
         }
 
-        mcu[3 * 8 * j + 3 * i + c] *= 1.0 / 4.0;
-        mcu[3 * 8 * j + 3 * i + c] *= (i == 0 ? 1.0 / sqrt(2) : 1);
-        mcu[3 * 8 * j + 3 * i + c] *= (i == 0 ? 1.0 / sqrt(2) : 1);
+        mcu_out[3 * 8 * j + 3 * i + c] *= 1.0 / 4.0;
+        mcu_out[3 * 8 * j + 3 * i + c] *= (i == 0 ? 1.0 / sqrt(2) : 1);
+        mcu_out[3 * 8 * j + 3 * i + c] *= (i == 0 ? 1.0 / sqrt(2) : 1);
       }
     }
   }
-
-  free(buf);
 }
 
 void display(uint16_t *mcu, int stride) {
